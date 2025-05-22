e22a08af0ff4b76e9652116b14012df8c9f9ecc5
MajoChi5 <A01712974@tec.mx>
2025-05-22 17:52:18 -0600

    Ticket.h

--- Ticket.h
+++ Ticket.h
@@ -1,1 +1,25 @@
+#ifndef TICKET_H
+#define TICKET_H
 
+#include <string>
+
+class Ticket {
+protected:
+    int price;
+    std::string code;
+    std::string date;
+
+public:
+    Ticket(int price, std::string code, std::string date);
+
+    virtual std::string showDetail() const;
+
+    int getPrice() const;
+    int getPrice(int price_discount) const;
+
+    std::string getCode() const;
+    std::string getDate() const;
+
+};
+
+#endif // TICKET_H
