23b1be531fcdae6c55bcc144cc966ee85b053f87
afloresmtz <a01708363@tec.mx>
2025-05-22 17:54:31 -0600

    Update VIPTicket.h

--- VIPTicket.h
+++ VIPTicket.h
@@ -1,1 +1,16 @@
+#ifndef VIPTICKET_H
+#define VIPTICKET_H
+
+#include <string>
+
+class VIPTicket : public Ticket {
+private:
+    std::string benefits;
+public:
+    VIPTicket(int price, std::string code, std::string date, std::string benefits);
+
+    std::string showDetails() override;
+};
+
+#endif
 
