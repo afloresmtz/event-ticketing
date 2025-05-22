0ce2708bcf06a14ccb1f17a4643e9dc3ca95ef39
MajoChi5 <A01712974@tec.mx>
2025-05-22 17:57:53 -0600

    Ticket.cpp

--- Ticket.cpp
+++ Ticket.cpp
@@ -1,1 +1,17 @@
+#include "Ticket.h"
+#include <sstream>
+
+Ticket::Ticket(int price, std::string code, std::string date)
+    : price(price), code(code), date(date) {}
+
+std::string Ticket::showDetail() const {
+    std::ostringstream detail;
+    detail <<"Ticket detail: \n"<<"======================\n"
+<< "Code: " << code << "\n"
+        << "Date: " << date << "\n"
+           << "Price: " << price << "\n"
+           <<"======================\n";
+    
+    return detail.str();
+}
 
