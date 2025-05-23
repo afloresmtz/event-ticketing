cfd1d93f5f49115b86b02a2dfbcded9e5337b752
MajoChi5 <A01712974@tec.mx>
2025-05-22 18:13:06 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
-
+#include "Ticket.cpp"
+#include "VIPTicket.cpp"
+#include "GeneralTicket.cpp"
+#include <iostream>
+int main(){
+    Ticket ticket(100, "TCK00", "2025-09-15");
+    std::cout << ticket.showDetail();
+    GeneralTicket generalTicket(100, "GEN123", "2025-09-15", "B");
+    std::cout << generalTicket.showDetail();
+  VIPTicket vipTicket(125, "VIP456", "2025-09-15", "Backstage access");
+  std::cout << vipTicket.showDetail();
+  return 0;
+  
+}
