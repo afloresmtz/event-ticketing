b5f8032dbbf58b056a4c4c90fb04b90c52aab10e
afloresmtz <a01708363@tec.mx>
2025-05-22 18:30:11 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include "VIPTicket.cpp"
 #include "GeneralTicket.cpp"
 #include <iostream>
+
 int main(){
     Ticket ticket(100, "TCK00", "2025-09-15");
     std::cout << ticket.showDetail();
     GeneralTicket generalTicket(100, "GEN123", "2025-09-15", "B");
     std::cout << generalTicket.showDetail();
-  VIPTicket vipTicket(125, "VIP456", "2025-09-15", "Backstage access");
-  std::cout << vipTicket.showDetail();
-  return 0;
-  
+    VIPTicket vipTicket(125, "VIP456", "2025-09-15", "Backstage access");
+    std::cout << vipTicket.showDetail();
+    return 0;
 }
