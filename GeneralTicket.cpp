3d136b27e149fda610a8a79ab6108eddd40dbb4c
afloresmtz <a01708363@tec.mx>
2025-05-22 18:03:05 -0600

    Update GeneralTicket.cpp

--- GeneralTicket.cpp
+++ GeneralTicket.cpp
@@ -1,1 +1,17 @@
+#include "GeneralTicket.h"
+#include <string>
 
+GeneralTicket::GeneralTicket(int price, std::string code, std::string date, std::string zone):
+    Ticket(price, code, date),zone(zone) { }
+
+std::string GeneralTicket::showDetail() {
+    std::ostringstream detail;
+    detail <<"General Ticket detail: \n"<<"======================\n"
+    << "Code: " << code << "\n"
+    << "Date: " << date << "\n"
+    << "Zone" << zone << "\n"
+    << "Final Price: " << price * (0.95) << "\n"
+    <<"======================\n";
+
+    return detail.str();
+}
