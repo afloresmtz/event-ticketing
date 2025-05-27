a60cc97ba40d3ec63f318a8294603917a0557f14
MajoChi5 <A01712974@tec.mx>
2025-05-26 18:14:07 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,12 @@ int main(){
 
 std::vector<Ticket*> tickets;
     tickets.push_back(new GeneralTicket(100, "GEN123", "2025-09-15", "B"));
+    tickets.push_back(new VIPTicket(100, "VIP434", "2025-09-15", "Backstage access"));
+    tickets.push_back(new GeneralTicket(100, "GEN124", "2025-09-15", "A"));
     tickets.push_back(new VIPTicket(100, "VIP456", "2025-09-15", "Backstage access"));
+
+    for (Ticket* ticket : tickets){
+        std::cout << ticket->showDetail();
+    }
     return 0;
 }
