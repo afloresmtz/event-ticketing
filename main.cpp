b96ccbc786beb83e05873cf52da86d42baca4662
MajoChi5 <A01712974@tec.mx>
2025-05-26 18:09:21 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 #include "VIPTicket.cpp"
 #include "GeneralTicket.cpp"
 #include <iostream>
+#include <vector>
 
 int main(){
     //Ticket ticket(100, "TCK00", "2025-09-15");
     //std::cout << ticket.showDetail();
     // this object cannot be created anymore now that Ticket is an abstract class
-    GeneralTicket generalTicket(100, "GEN123", "2025-09-15", "B");
-    std::cout << generalTicket.showDetail();
-    VIPTicket vipTicket(100, "VIP456", "2025-09-15", "Backstage access");
-    std::cout << vipTicket.showDetail();
+
+std::vector<Ticket*> tickets;
+    tickets.push_back(new GeneralTicket(100, "GEN123", "2025-09-15", "B"));
+    tickets.push_back(new VIPTicket(100, "VIP456", "2025-09-15", "Backstage access"));
     return 0;
 }
